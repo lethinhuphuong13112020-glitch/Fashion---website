b6728aecef19f1b13ce4c3428bee9e0611b2e99c
lethinhuphuong13112020-glitch <lethinhuphuong13112020@gmail.com>
2026-04-02 14:13:55 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>Web bán hàng thời trang</title>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="header">
+        <p>This is header</p>
+    </div>
+    <div class="menu">
+        <p>menu</p>
+    </div>
+    <div class="main">
+        
+        <div class="sidebar">
+            Sidebar
+    </div>
+    <div class="main content">
+        Main content
+    </div>
+    <div class="footer">
+        <p>footer</p>
+    </div>
+</body>
+</html>
